85b217b63ca71676eaa309433c1f8df7eed8a274
triberraar <geertolaerts@gmail.com>
2025-10-17 16:49:59 +0900

    zip controller

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -1,13 +1,25 @@
 package org.example.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import javax.inject.Named;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.MediaTypeNames;
 import com.linecorp.armeria.server.annotation.Blocking;
 import com.linecorp.armeria.server.annotation.Get;
 import com.linecorp.armeria.server.annotation.Param;
 import com.linecorp.armeria.server.annotation.PathPrefix;
+import com.linecorp.armeria.server.annotation.Produces;
 import com.linecorp.armeria.server.docs.DocServiceBuilder;
 
 import lombok.SneakyThrows;
@@ -35,6 +47,53 @@ public class TestController {
     public String third(@Param Integer first, @Param String second) {
         log.info("third");
         return "third";
+    }
+
+    @Get("/zip1")
+    @Produces(MediaTypeNames.ZIP)
+    public byte[] zip1() {
+        var bytes = "sdf".getBytes();
+        try (ByteArrayOutputStream bytesOutputStream = new ByteArrayOutputStream();
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(bytesOutputStream);
+             ZipOutputStream zipOutputStream = new ZipOutputStream(bufferedOutputStream,
+                                                                   StandardCharsets.UTF_8)) {
+
+            putAndWriteFile(zipOutputStream, "", bytes, "test.txt");
+
+            zipOutputStream.close();
+            return bytesOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Get("/zip2")
+    @Produces(MediaTypeNames.ZIP)
+    public HttpResponse zip2() {
+        var bytes = "sdf".getBytes();
+        try (ByteArrayOutputStream bytesOutputStream = new ByteArrayOutputStream();
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(bytesOutputStream);
+             ZipOutputStream zipOutputStream = new ZipOutputStream(bufferedOutputStream,
+                                                                   StandardCharsets.UTF_8)) {
+
+            putAndWriteFile(zipOutputStream, "", bytes, "test.txt");
+
+            zipOutputStream.close();
+            return HttpResponse.of(
+                    HttpStatus.OK,
+                    MediaType.ZIP,
+                    bytesOutputStream.toByteArray()
+            );
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static void putAndWriteFile(ZipOutputStream zipOutputStream, String directoryName,
+                                        byte[] bytes, String fileName) throws IOException {
+        zipOutputStream.putNextEntry(new ZipEntry(directoryName + fileName));
+        zipOutputStream.write(bytes);
+        zipOutputStream.closeEntry();
     }
 
     @SneakyThrows
